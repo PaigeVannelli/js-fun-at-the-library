96c2742aeedd99c1edd4f1f463427a7e3ca71d32
Paige Vannelli <paigevannelli@gmail.com>
2020-12-03 08:55:05 -0700

    Add answers to half of book

--- src/book.js
+++ src/book.js
@@ -1,12 +1,37 @@
-function createTitle() {
+function createTitle(title) {
+  return 'The ' + title;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return character
+}
+
+function saveReview(specificReview, reviews) {
+  if (!reviews.includes(specificReview)){
+    reviews.push(specificReview)
+  }
+  // reviews.includes(specificReview) ? 'isTrue' : reviews.push(specificReview)
+};
+
+function calculatePageCount(bookTitle) {
+  var length = bookTitle.length;
+  return length * 20;
+};
+
+function writeBook() {
   
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
   // editBook
 }
