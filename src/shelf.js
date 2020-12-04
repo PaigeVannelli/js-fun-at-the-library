40e2a755ff11d37c925d16138d67fa9d4c665ee1
Paige Vannelli <paigevannelli@gmail.com>
2020-12-03 18:07:31 -0700

    Add three functions to shelf, shelfBook, unshelfBook and listTitles

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,33 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  }
+};
+
+function unshelfBook(title, shelf) {
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      shelf.splice(i, 1);
+    };
+  }
+};
+
+function listTitles(shelf) {
+//   var listOfTitles = [];
+//   for (i = 0; i < shelf.length; i++) {
+//     listOfTitles.push(shelf[i].title);
+//   }
+//   console.log(listOfTitles);
+// };
+
+  return `${shelf[0].title}, ${shelf[1].title}, ${shelf[2].title}`;
+}
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
