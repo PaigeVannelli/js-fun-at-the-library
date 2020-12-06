5fca5eb32bd9088e4e8ec08f35a9b1de0d4e5dab
Paige Vannelli <paigevannelli@gmail.com>
2020-12-05 23:34:15 -0700

    Start refactor of findBook method

--- src/librarian.js
+++ src/librarian.js
@@ -2,14 +2,30 @@ class Librarian {
   constructor(name, library) {
     this.name = name;
     this.library = library;
-  }
+  };
   greetPatron(name, morning) {
-    if (morning === true ) {
-      return `Good morning, ${name}!`
+    if (morning) {
+      return `Good morning, ${name}!`;
     } else {
-    return `Hello, ${name}!`
-    }
+      return `Hello, ${name}!`;
+    };
   }
+
+  findBook(title) {
+    var libraryShelves = Object.keys(this.library.shelves)
+    for (var i = 0; i < this.library.shelves.length; i++) {
+    // }
+    //I have an array of the shelves that I need to iterate through 
+// take a look at every shelf in the Library/
+// iterate through each shelfs auroraLibrary
+//change your variable outside of it tpo equal true and print
+  }
+
+// };
+
+
+
+
   // findBook(title) {
   //   for (var i = 0; i < this.library.shelves.fantasy.length; i++ ) {
   //     if (this.library.shelves.fantasy[i].title === title) {
@@ -19,15 +35,15 @@ class Librarian {
   //       return `Sorry, we do not have ${title}`;
   //     };
   //   }
-
-  findBook(title) {
-    console.log(object.keys(this.library.shelves))
-  }
-
-  calculateLateFee(daysLate) {
-    return Math.ceil(daysLate * .25)
-  }
-};
-
+//
+//   findBook(title) {
+//     console.log(object.keys(this.library.shelves))
+//   }
+//
+//   calculateLateFee(daysLate) {
+//     return Math.ceil(daysLate * .25)
+//   }
+// };
+}
 
 module.exports = Librarian;
