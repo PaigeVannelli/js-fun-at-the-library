c5b7d599fa93b31f6660de9715bf204108b94685
Paige Vannelli <paigevannelli@gmail.com>
2020-12-06 12:03:03 -0700

    Refactor findBook function in librarian

--- src/librarian.js
+++ src/librarian.js
@@ -12,38 +12,23 @@ class Librarian {
   }
 
   findBook(title) {
-    var libraryShelves = Object.keys(this.library.shelves)
-    for (var i = 0; i < this.library.shelves.length; i++) {
-    // }
-    //I have an array of the shelves that I need to iterate through 
-// take a look at every shelf in the Library/
-// iterate through each shelfs auroraLibrary
-//change your variable outside of it tpo equal true and print
-  }
+    var libraryShelves = Object.keys(this.library.shelves);
+    for (var i = 0; i < libraryShelves.length; i++) {
+      for (var j = 0; j < this.library.shelves[libraryShelves[i]].length; j++) {
+        if (this.library.shelves[libraryShelves[i]][j].title === title) {
+          this.library.shelves[libraryShelves[i]].splice([j], 1)
+          return `Yes, we have ${title}`;
+        } else {
+          return `Sorry, we do not have ${title}`;
+        };
+      };
+    };
+  };
 
-// };
+  calculateLateFee(daysLate) {
+    return Math.ceil(daysLate * .25);
+  };
+};
 
-
-
-
-  // findBook(title) {
-  //   for (var i = 0; i < this.library.shelves.fantasy.length; i++ ) {
-  //     if (this.library.shelves.fantasy[i].title === title) {
-  //       this.library.shelves.fantasy.splice([i], 1);
-  //       return `Yes, we have ${title}`;
-  //     } else {
-  //       return `Sorry, we do not have ${title}`;
-  //     };
-  //   }
-//
-//   findBook(title) {
-//     console.log(object.keys(this.library.shelves))
-//   }
-//
-//   calculateLateFee(daysLate) {
-//     return Math.ceil(daysLate * .25)
-//   }
-// };
-}
 
 module.exports = Librarian;
