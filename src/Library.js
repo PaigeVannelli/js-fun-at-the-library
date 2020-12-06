438470e35a72d9fc4f0b8b6c5581319a69a53aff
Paige Vannelli <paigevannelli@gmail.com>
2020-12-05 22:55:30 -0700

    Refactor addBook function in Library

--- src/Library.js
+++ src/Library.js
@@ -11,28 +11,17 @@ function createLibrary(name) {
 };
 
 function addBook(library, book) {
-  if (book.genre === "fantasy") {
-    library.shelves.fantasy.push(book);
-  } else if (book.genre === "fiction") {
-      library.shelves.fiction.push(book);
-  } else if (book.genre === "nonFiction") {
-      library.shelves.nonFiction.push(book)
-  } else {
-  }
-}
-
-//SPACING???
-
+    library.shelves[book.genre].push(book)
+  };
 
 function checkoutBook(library, title, genre) {
- //Iterate through the three shelves and see if title is strictly ==
  for (i = 0; i < library.shelves[genre].length; i++) {
    if (library.shelves[genre][i].title === title) {
-     library.shelves[genre].splice(i, 1)
-     return `You have now checked out ${title} from the ${library.name}`
-   }
- }
- return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+     library.shelves[genre].splice(i, 1);
+     return `You have now checked out ${title} from the ${library.name}`;
+   };
+ };
+ return `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
 };
 
 module.exports = {
